acd9b0b56e59b38bbec4c6ab43a8b37320aa0b7c
Christopher Yee <chris.anthony.yee@gmail.com>
2017-10-16 01:17:09 -0700

    assignment 7

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,16 +2,52 @@
      function SongPlayer() {
           var SongPlayer = {};
 
+          var currentSong = null;
+          //@desc Buzz object audio file
+          // @type {Object}
+
+          var currentBuzzObject = null;
+
+          // @fucntion setSong
+          // @desc Stops currently palying song and loads new audio file as currentBuzzObject
+          //@param {Object} song
+
+          var setSong = function(song) {
+            if (currentBuzzObject) {
+              currentBuzzObject.stop();
+              currentSong.playing = null;
+              }
+
+            var playSong = function(song)  {
+              if (currentBuzzObject){
+              currentBuzzObject.play();
+              song.playing = true;
+              }
+            }
+
+          currentBuzzObject = new buzz.sound(song.audioUrl, {
+              formats: ['mp3'],
+                preload: true
+              });
+
+                currentSong = song;
+              };
+
+          SongPlayer.currentSong = null;
+
           SongPlayer.play = function(song) {
-console.log('song;', song);
-                   var currentBuzzObject = new buzz.sound(song.audioUrl, {
-                       formats: ['mp3'],
-                       preload: true
+            if (currentSong !== song) {
 
-                   });
-
+                  setSong(song);
                    currentBuzzObject.play();
+                   song.playing = true;
+                 }
                };
+
+        SongPlayer.pause = function(song) {
+              currentBuzzObject.pause();
+              song.playing = false;
+              };
 
 
 
